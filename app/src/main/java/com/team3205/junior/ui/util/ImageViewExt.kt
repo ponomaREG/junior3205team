1b3fb1ce4dc672eaa1fbb05f97383a7239caa9a7
ponomaREG <ponomarcomru@gmail.com>
2021-08-05 13:29:27 +0300

    Add documentation of Util ImageViewExt.kt

--- app/src/main/java/com/team3205/junior/ui/util/ImageViewExt.kt
+++ app/src/main/java/com/team3205/junior/ui/util/ImageViewExt.kt
@@ -3,6 +3,9 @@ package com.team3205.junior.ui.util
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
+/**
+ * Extension для скачивания изображения в ImageView
+ */
 fun ImageView.loadImage(url: String){
     Glide.with(this)
         .load(url)
