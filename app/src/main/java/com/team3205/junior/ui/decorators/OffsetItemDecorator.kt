e440ab172dd7ea2ba068e49e7563435bb4d0510e
ponomaREG <ponomarcomru@gmail.com>
2021-08-05 12:20:30 +0300

    Create OffsetItemDecorator for offsets in recycler view

---
+++ app/src/main/java/com/team3205/junior/ui/decorators/OffsetItemDecorator.kt
@@ -0,0 +1,22 @@
+package com.team3205.junior.ui.decorators
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class OffsetItemDecorator(
+    val left: Int,
+    val right: Int,
+    val top: Int,
+    val bottom: Int
+): RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.set(left, top, right, bottom)
+    }
+}
