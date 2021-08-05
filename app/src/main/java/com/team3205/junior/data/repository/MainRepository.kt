82f29959d753b429451f7b38618893969b2d603c
ponomaREG <ponomarcomru@gmail.com>
2021-08-05 12:56:55 +0300

    Add documentation of MainRepository

--- app/src/main/java/com/team3205/junior/data/repository/MainRepository.kt
+++ app/src/main/java/com/team3205/junior/data/repository/MainRepository.kt
@@ -1,7 +1,5 @@
 package com.team3205.junior.data.repository
 
-import android.accounts.NetworkErrorException
-import android.util.Log
 import com.team3205.junior.data.db.AppDb
 import com.team3205.junior.data.db.entities.RecentSearchDB
 import com.team3205.junior.data.mappers.*
@@ -9,12 +7,21 @@ import com.team3205.junior.data.network.service.RepositoryService
 import com.team3205.junior.data.repository.entities.RecentSearch
 import com.team3205.junior.data.repository.entities.Repository
 import com.team3205.junior.data.repository.entities.User
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import java.lang.IllegalStateException
 import java.util.*
 import javax.inject.Inject
 
+/**
+ * Репозиторий для получения данных
+ * @param repositoryService - сервис для получения данных с API
+ * @param appDb - база данных
+ * @see RepositoryService
+ * @see AppDb
+ * @see RepositoryMapper
+ * @see UserMapper
+ * @see RecentSearchMapper
+ * @see RepositoryDBMapper
+ * @see UserDBMapper
+ */
 class MainRepository @Inject constructor(
     private val repositoryService: RepositoryService,
     private val appDb: AppDb
@@ -25,18 +32,32 @@ class MainRepository @Inject constructor(
     private val repositoryDbMapper = RepositoryDBMapper()
     private val userDBMapper = UserDBMapper()
 
-    suspend fun getSavedRepositoriesByUserName(username: String): List<Repository> {
+    /**
+     * Получение сохраненных репозиториев по имени пользователя
+     * @param username - поисковой запрос пользователя
+     * @return - лист с репозиториями
+     */
+    suspend fun getSavedRepositoriesFromDatabaseByUserName(username: String): List<Repository> {
         val savedUser = appDb.getUserDAO().getSavedUserByUserName(username.toUpperCase(Locale.ROOT))
         return if(savedUser == null) emptyList()
                else appDb.getRepositoryDAO().getSavedRepositoriesByOwnerId(savedUser.id)
                     .map { repositoryMapper.fromRepositoryDB(it,userMapper.fromUserDB(savedUser)) }
     }
 
+    /**
+     * Получение репозиториев по имени пользователя из сети
+     * @param username - поисковой запрос пользователя
+     * @return - лист с репозиториями
+     */
     suspend fun getRepositoriesFromNetworkByUserName(username: String): List<Repository> {
         val repositoriesFromNetwork = repositoryService.getRepositoriesByUserName(username)
         return repositoriesFromNetwork.map { repositoryMapper.fromRepositoryResponse(it) }
     }
 
+    /**
+     * Получение сохраненных репозиториев
+     * @return - лист с репозиториями
+     */
     suspend fun getSavedRepositories(): List<Repository>{
         return appDb.getRepositoryDAO().getSavedRepositories()
             .map {
@@ -45,23 +66,42 @@ class MainRepository @Inject constructor(
                  }
     }
 
+    /**
+     * Получение истории запросов
+     * @return - лист ранних запросов
+     */
     suspend fun getRecentSearches(): List<RecentSearch>{
         return appDb.getRecentSearchDAO().getRecentSearches(5)
             .map { recentSearchesMapper.fromRecentSearchDB(it) }
     }
 
+    /**
+     * Сохранение раннего поиска
+     * @param username - поисковой запрос пользователя
+     */
     suspend fun saveRecentSearch(username: String){
         appDb.getRecentSearchDAO().insert(RecentSearchDB(0,username))
     }
 
+    /**
+     * Сохранение репозитория
+     * @param repository - репозиторий
+     */
     suspend fun saveRepo(repository: Repository){
         appDb.getRepositoryDAO().insert(repositoryDbMapper.fromRepository(repository))
     }
 
+    /**
+     * Сохранение пользователя
+     * @param user - пользователь
+     */
     suspend fun saveUser(user:User){
         appDb.getUserDAO().insert(userDBMapper.fromUser(user))
     }
 
+    /**
+     * Очистка истории поиска
+     */
     suspend fun clearRecentSearches(){
         appDb.getRecentSearchDAO().deleteAll()
     }
